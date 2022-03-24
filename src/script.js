e9003be97641beb07beec0e75f94e2a61425f465
cumulonimbusbaby <cumulonimbussbaby@gmail.com>
2022-03-24 17:46:42 -0400

    minor edits to html and js

--- src/script.js
+++ src/script.js
@@ -47,7 +47,7 @@ function displayTemprature(response) {
 
 function search(city) {
   let apiKey = "d3592968d288237ab5de304e493c66f3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemprature);
 }
 
