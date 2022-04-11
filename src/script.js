090189ab7a5fd233cd431ab11b4b99c367cf62db
cumulonimbusbaby <cumulonimbussbaby@gmail.com>
2022-04-11 15:46:04 -0400

    MOVING HTML TO JS

--- src/script.js
+++ src/script.js
@@ -21,6 +21,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = `
+  <div class="row">
+    <div class="col-2">
+        <div class="weather-forecast-date">
+          Thursday
+        </div>
+          <br>
+        <img src="http://openweathermap.org/img/wn/04d@2x.png" 
+          alt=""
+          width="56">
+          <br>
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">
+              18 / 
+          </span>
+          <span class="weather-forecast-temperature-min">
+              12 
+          </span>
+        </div>
+     </div>
+  </div>
+  `;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
